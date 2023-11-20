eb35997d75c5f5a54ecb7f121a15664f425a7c45
Niklas Kerkhoff <niklas.kerkhoff@icloud.com>
2023-11-20 19:31:57 +0100

    database

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,11 @@ repositories {
 }
 
 dependencies {
-    //implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    //runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
